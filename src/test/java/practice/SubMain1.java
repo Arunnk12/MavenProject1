801e4731fe53765514a7aff11f98fa6de9280dda
Arun Kumar N K <Arun Kumar N K@LAPTOP-U43VOP2V>
2022-06-16 16:43:22 +0530

    mvn classes

---
+++ src/test/java/practice/SubMain1.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class SubMain1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("execute maven project");
+	}
+
+}
